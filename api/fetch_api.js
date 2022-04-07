63bf8a44c086f6b74398ebc44bafb3164d315ff0
shivi11g <99272893+shivi11g@users.noreply.github.com>
2022-04-08 00:12:18 +0530

    Update fetch_api.js

--- api/fetch_api.js
+++ api/fetch_api.js
@@ -12,19 +12,6 @@ function getData(){
 }
 getData();
 
-// function appendData(){
-// var mainContainer = document.getElementById("mydata");
-// for(var i; i< data.length; i++){
-// 	var div = document.createElement("div");
-// 	var div2 = document.createElement("div2");
-// 	div.innerHTML = 'code: ' + data[i].code;
-// 	div2.innerHTML = 'name:' + data[i].name;
-// 	//div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-// 	mainContainer.appendChild(div);
-// 	mainContainer.appendChild(div2);
-// }
-// }
-// // appendData();
 
 
 
